283d96506a68d0e931cdcc723820a900fcd06c2c
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2023-07-24 09:24:17 +0530

    Create tedit.c

---
+++ tedit.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+
+/*** Defination ***/
+
+#define 
